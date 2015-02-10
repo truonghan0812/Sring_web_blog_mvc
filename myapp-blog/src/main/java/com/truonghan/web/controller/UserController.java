7066ae752253a1be4c9710e38e664f7832b5d94a
truonghan <truonghan@truonghan-PC>
2015-02-10 15:02:18 +0700

    part 18

--- myapp-blog/src/main/java/com/truonghan/web/controller/UserController.java
+++ myapp-blog/src/main/java/com/truonghan/web/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.truonghan.web.controller;
-"Eclipse Color Theme 
+
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.truonghan.backend.domain.User;
 import com.truonghan.backend.services.UserService;
 
 @Controller
@@ -14,6 +18,13 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	//This create a user model for first request get method
+	@ModelAttribute("user")
+	public User getUser(){
+		return new User();
+	}
+	
 	
 	@RequestMapping(value="/users")
 	public String users(Model model){
@@ -31,6 +42,22 @@ public class UserController {
 	
 	@RequestMapping(value="/register")
 	public String ShowRegister(){
+		System.out.println("----------------------Getting in Register Controller!-----------------------------");
 		return "user-register";
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public String saveUser(@ModelAttribute("user") User user){
+		
+//		if(rs.hasErrors()){
+//			System.out.println("-------Binding has errors---------");
+//			return "user-register";
+//		}
+//		else{
+			System.out.println("-------Saving user---------");
+			userService.save(user);
+			return "redirect:/users.html";
+//		}
+		
 	}
 }
--- myapp-blog/src/main/webapp/WEB-INF/views/user-register.jsp
+++ myapp-blog/src/main/webapp/WEB-INF/views/user-register.jsp
@@ -2,7 +2,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ include file="../layout/taglib.jsp"%>
-
+<br>
 <spform:form class="form-horizontal" commandName="user">
 	<div class="form-group">
 		<label for="namelabel" class="col-sm-2 control-label">User
@@ -15,6 +15,12 @@
 		<label for="emaillabel" class="col-sm-2 control-label">Email:</label>
 		<div class="col-sm-10">
 			<spform:input class="form-control" path="email"></spform:input>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="emaillabel" class="col-sm-2 control-label">Password:</label>
+		<div class="col-sm-10">
+			<spform:password class="form-control" path="email"></spform:password>
 		</div>
 	</div>
 	<div class="form-group">
